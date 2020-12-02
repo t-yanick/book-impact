07190a2c9fbc146bf88df772cc0b2608a0ad8c3e
Yanick <tyankee15@gmail.com>
2020-12-02 20:48:33 -0300

    Scaffold Opinion model and dependecies

---
+++ app/views/opinions/_opinion.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! opinion, :id, :feedback, :created_at, :updated_at
+json.url opinion_url(opinion, format: :json)
---
+++ db/migrate/20201202225319_create_opinions.rb
@@ -0,0 +1,12 @@
+class CreateOpinions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :opinions do |t|
+      t.integer :user_id
+      t.text :feedback
+
+      t.timestamps
+    end
+
+    add_index :opinions, :user_id
+  end
+end
