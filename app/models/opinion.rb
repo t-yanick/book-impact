a8fbad9f6e14d439dc1e7ebe12ee2f2fcfb5cc76
Yanick <tyankee15@gmail.com>
2020-12-03 05:25:12 -0300

    Add associations for user and opinion models

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,2 +1,7 @@
 class Opinion < ApplicationRecord
+  validates :feedback, presence: true, length: { maximum: 1000,
+                                                too_long: '1000 characters in post is the maximum allowed.' }
+
+  belongs_to :user
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   root 'opinions#index'
+
+  devise_for :users
+
+  resources :users, only: [:index, :show] 
   resources :opinions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
