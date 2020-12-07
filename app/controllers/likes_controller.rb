b9b73fcc670bbdb6f0bf1471195153e4050d0382
Yanick <tyankee15@gmail.com>
2020-12-07 16:12:36 -0300

    Add Likes control with create and destroy methods

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,21 @@
+class LikesController < ApplicationController
+	def create
+    @like = current_user.likes.new(opinion_id: params[:opinion_id])
+
+    if @like.save
+      redirect_to opinions_path, notice: 'You liked a review.'
+    else
+      redirect_to opinions_path, alert: 'You cannot like this review.'
+    end
+  end
+
+  def destroy
+    like = Like.find_by(id: params[:id], user: current_user, opinion_id: params[:opinion_id])
+    if like
+      like.destroy
+      redirect_to opinions_path, notice: 'You disliked a review.'
+    else
+      redirect_to opinions_path, alert: 'You cannot dislike a review that you did not like before.'
+    end
+  end
+end
