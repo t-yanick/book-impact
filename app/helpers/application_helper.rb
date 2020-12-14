de568e3f43dd3df7413491b3d3ddac6dace6a520
Yanick <tyankee15@gmail.com>
2020-12-14 18:56:47 -0300

    Install action_text for rich text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,11 +9,25 @@ module ApplicationHelper
 
   def signed_in?
     if user_signed_in?
-      ("#{link_to current_user.name, user_path(current_user), class: 'button is-info'}" <<
-       "#{link_to 'Logout', destroy_user_session_path, method: :delete, class: 'button is-info'}").html_safe
+      "#{link_to 'New Book Review', new_opinion_path, class: 'btn btn-light my-2 my-sm-0 logs'}" \
+        "#{link_to 'Edit Profile', edit_user_registration_path, class: 'btn text-light m-2 logs'}" \
+       "#{link_to 'Logout', destroy_user_session_path, method: :delete, class: 'btn text-light m-2 logs'}".html_safe
     else
-      ("#{link_to 'Sing In', new_user_session_path, class: 'button is-info'}" <<
-       "#{link_to 'Sing Up', new_user_registration_path, class: 'button is-info'}").html_safe
+      "#{link_to 'Sign In', new_user_session_path, class: 'btn text-light m-2 logs'}" \
+       "#{link_to 'Sing Up', new_user_registration_path, class: 'btn text-light m-2 logs'}".html_safe
+    end
+  end
+
+  def like_or_dislike_btn(opinion)
+    like = Like.find_by(opinion: opinion, user: current_user)
+    if like
+      link_to opinion_like_path(id: like.id, opinion_id: opinion.id), method: :delete, class: 'like-button' do
+        fa_icon 'thumbs-down'
+      end
+    else
+      link_to opinion_likes_path(opinion_id: opinion.id), method: :post, class: 'like-button' do
+        fa_icon 'thumbs-up'
+      end
     end
   end
 end
